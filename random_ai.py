396b624516fad2dc3ddd5cdf498d687edecbe33e
NoName64Bits <ciobanulaur@gmail.com>
2016-09-07 23:38:56 +0300

    second commit

--- random_ai.py
+++ random_ai.py
@@ -4,9 +4,13 @@ import random
 
 
 class Player(PlayerAI):
+    targetFood = None
+
     def __init__(self):
         super(Player, self).__init__('Laur')
 
     def update(self, player_positions, food_positions):
-        food = random.choice(food_positions)
-        return food[0] - self.position[0], food[1] - self.position[1]
+        if targetFood != None:
+            food = random.choice(food_positions)
+
+        return targetFood[0] - self.position[0], targetFood[1] - self.position[1]
